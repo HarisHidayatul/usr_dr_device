0898e5cf5e4440154dff902905a39029c28a15f2
unknown <hidayatullohharis@gmail.com>
2023-08-03 15:02:12 +0700

    reading data

--- modbusServer.py
+++ modbusServer.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 import json
 
 # Import library untuk Modbus
@@ -45,7 +45,7 @@ def read_input_register(unit_id):
             # Menambahkan penamaan untuk setiap elemen dalam array
             
             named_data.update({
-                "Water inlet temperature setting in cooling mode": result[0],
+                "Water Inlet Temperature Setting In Cooling Mode": result[0],
             })
         else:
             print("Gagal membaca data dari perangkat Modbus.")
@@ -55,7 +55,7 @@ def read_input_register(unit_id):
         result = client.read_input_registers(5006, 1)
         if result:
             named_data.update({
-                "Water outlet temperature setting in cooling mode": result[0]
+                "Water Outlet Temperature Setting In Cooling Mode": result[0]
             })
         else:
             print("Gagal membaca data dari perangkat Modbus.")
@@ -64,7 +64,7 @@ def read_input_register(unit_id):
         result = client.read_input_registers(5011, 1)
         if result:
             named_data.update({
-                "Compressor output ratio": result[0]
+                "Compressor Output Ratio": result[0]
             })
         else:
             print("Gagal membaca data dari perangkat Modbus.")
@@ -81,64 +81,43 @@ def read_input_register(unit_id):
         return jsonify({"error": str(e)})
 
 @app.route('/read_holding_register/<int:unit_id>', methods=['GET'])
-def read_input_register_1(unit_id):
+def read_holding_register(unit_id):
     try:
         client = ModbusClient(host=HOST, port=PORT, unit_id=unit_id)
         
         # Lakukan operasi pembacaan data
         # client.write_single_register(2,120)
-        result = client.read_holding_registers(0, 8)
+        result = client.read_holding_registers(1, 5)
         named_data = {}
         if result:
             print("Data yang dibaca dari perangkat Modbus:", result)
             # Menambahkan penamaan untuk setiap elemen dalam array
             
             named_data.update({
-                "Unit Fault": result[0],
-                "System 1 Fault": result[1],
-                "System 2 Fault": result[2],
-                "Ambient Air Temperature": result[3],
-                "Water Inlet Temperature": result[4],
-                "Water Outlet Temperature": result[5],
-                "System 1 and 2 Fault": result[6],
-                "Water Outlet Of Hot Water Side Temperature": result[7]
+                "ON/OFF Setting": result[0],
+                "Running Mode Setting": result[1],
+                "Water Inlet Temperature In Cooling Mode": result[2],
+                "Water Inlet Temperature In Heating Mode": result[3],
+                "Water Tank Temperature Setting": result[4]
             })
             # Mengubah data menjadi format JSON
         else:
             print("Gagal membaca data dari perangkat Modbus.")
             data_json = json.dumps({})  # Data kosong jika gagal
             
-        result = client.read_input_registers(5002, 1)
+        result = client.read_holding_registers(22, 2)
         if result:
             print("Data yang dibaca dari perangkat Modbus:", result)
             # Menambahkan penamaan untuk setiap elemen dalam array
             
             named_data.update({
-                "Water inlet temperature setting in cooling mode": result[0],
+                "Water Outlet Temperature Setting In Cooling Mode": result[0],
+                "Water Outlet Temperature Setting In Heating Mode": result[1]
             })
         else:
             print("Gagal membaca data dari perangkat Modbus.")
             data_json = json.dumps({})  # Data kosong jika gagal
         
-        
-        result = client.read_input_registers(5006, 1)
-        if result:
-            named_data.update({
-                "Water outlet temperature setting in cooling mode": result[0]
-            })
-        else:
-            print("Gagal membaca data dari perangkat Modbus.")
-            data_json = json.dumps({})  # Data kosong jika gagal
-        
-        result = client.read_input_registers(5011, 1)
-        if result:
-            named_data.update({
-                "Compressor output ratio": result[0]
-            })
-        else:
-            print("Gagal membaca data dari perangkat Modbus.")
-            data_json = json.dumps({})  # Data kosong jika gagal
-
         data_json = json.dumps(named_data)
         client.close()
         return jsonify({
